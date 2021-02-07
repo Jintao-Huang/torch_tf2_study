e03ced8b83d83cda2d24079ded259acffa80e4f9
Jintao Huang <hjt_study@qq.com>
2021-02-08 00:35:14 +0800

    small changes

--- 分类_examples/cnn_mnist.py
+++ 分类_examples/cnn_mnist.py
@@ -1,12 +1,13 @@
 # Author: Jintao Huang
-# Time: 2020-5-16
+# Time: 2020-5-14
 
-from torchvision.datasets.cifar import CIFAR10
+from torchvision.datasets.mnist import MNIST
 import torchvision.transforms.transforms as trans
 from torch.utils.data import DataLoader
 import torch.nn as nn
 import torch
 import time
+import os
 from tensorboardX import SummaryWriter
 
 
@@ -21,7 +22,7 @@ class ConvBNReLU(nn.Sequential):
 
 class ConvBlock(nn.Sequential):
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding, bias):
-        super().__init__(
+        super(ConvBlock, self).__init__(
             ConvBNReLU(in_channels, out_channels, kernel_size, 1, padding, bias=bias),
             ConvBNReLU(out_channels, out_channels, kernel_size, 1, padding, bias=bias),
             ConvBNReLU(out_channels, out_channels, kernel_size, stride, padding, bias=bias)
@@ -33,15 +34,14 @@ class SimpleCNN(nn.Module):
 
     def __init__(self):
         super(SimpleCNN, self).__init__()
-        self.features = nn.Sequential(
-            ConvBlock(3, 16, 3, 2, 1, False),  # 16
-            ConvBlock(16, 32, 3, 2, 1, False),  # 8
-            ConvBlock(32, 64, 3, 2, 1, False)  # 4
+        self.features = nn.Sequential(  # 特征提取器
+            ConvBlock(1, 16, 3, 2, 1, False),  # 14
+            ConvBlock(16, 64, 3, 2, 1, False)  # 7
         )
-        self.classifier = nn.Sequential(
-            nn.Linear(4 * 4 * 64, 512),
+        self.classifier = nn.Sequential(  # 分类器
+            nn.Linear(7 * 7 * 64, 512),
             nn.ReLU(inplace=True),
-            nn.Dropout(0.2),  # drop_p
+            nn.Dropout(0.25),  # drop_p
             nn.Linear(512, 10),
         )
 
@@ -52,40 +52,23 @@ class SimpleCNN(nn.Module):
         return x
 
 
+def save_params(model, filename):
+    torch.save(model.state_dict(), filename)
+
+
+def load_params(model, filepath):
+    return model.load_state_dict(torch.load(filepath))
+
+
 def get_acc(pred, label):
     pred = torch.argmax(pred, dim=1)
     return torch.mean((pred == label).float())
 
 
-def save_params(model, filename):
-    torch.save(model.state_dict(), filename)
-
-
-def lr_schedule(optim, epoch, milestones, decline_rate=0.1):
-    """学习率下降(Decline in learning rate)"""
-
-    def insert_index(x, arr):
-        """arr是一个从小到大的arr, x插入arr应该在的下标(二分查找). 你可以改成顺序查找."""
-
-        lo, hi = 0, len(arr)
-        while lo < hi:
-            mid = (lo + hi) // 2
-            if x < arr[mid]:
-                hi = mid
-            else:
-                lo = mid + 1
-        return lo
-
-    default_lr = optim.defaults['lr']
-    lr = default_lr * decline_rate ** insert_index(epoch, milestones)
-    optim.param_groups[0]['lr'] = lr
-    return lr
-
-
-def train(model, loss_fn, optim, train_loader, epoch, device, writer=None):
+def train(model, train_loader, loss_fn, optim, lr_scheduler, writer, epoch, device):
     for i in range(epoch):
         loss_total, acc_total, start_time = 0., 0., time.time()
-        lr = lr_schedule(optim, i, (10, 15), decline_rate=0.1)
+        lr_scheduler.step(i)
         for j, (data, label) in enumerate(train_loader):
             data, label = data.to(device), label.to(device)
             pred = model(data)
@@ -97,25 +80,20 @@ def train(model, loss_fn, optim, train_loader, epoch, device, writer=None):
             acc = get_acc(pred, label)
             loss_total += loss.item()
             acc_total += acc.item()
-            if j % 10 == 0:
-                loss_mean = loss_total / (j + 1)
-                acc_mean = acc_total / (j + 1)
+            writer.add_scalar("loss", loss, i * len(train_loader) + j + 1)
+            writer.add_scalar("acc", acc, i * len(train_loader) + j + 1)
+            if j % 20 == 0:
+                loss = loss_total / (j + 1)
+                acc = acc_total / (j + 1)
                 end_time = time.time()
-                if writer is not None:
-                    writer.add_scalar("loss", loss, i * len(train_loader) + j + 1)
-                    writer.add_scalar("acc", acc, i * len(train_loader) + j + 1)
-                print("\r>> Epoch: %d[%d/%d]| Loss: %.6f| Acc: %.2f%%| Time: %.4f| LR: %g" %
-                      (i, j + 1, len(train_loader), loss_mean, acc_mean * 100, end_time - start_time, lr), end="")
+                print("\r>> Epoch: %d[%d/%d]|| Loss: %.6f| Acc: %.2f%%| Time: %.4f" %
+                      (i, j + 1, len(train_loader), loss, acc * 100, end_time - start_time), end="")
         else:
-            loss_mean = loss_total / (j + 1)
-            acc_mean = acc_total / (j + 1)
+            loss = loss_total / (j + 1)
+            acc = acc_total / (j + 1)
             end_time = time.time()
-            if writer is not None:
-                writer.add_scalar("loss", loss, i * len(train_loader) + j + 1)
-                writer.add_scalar("acc", acc, i * len(train_loader) + j + 1)
-                writer.flush()
-            print("\r>> Epoch: %d[%d/%d]| Loss: %.6f| Acc: %.2f%%| Time: %.4f| LR: %g" %
-                  (i, j + 1, len(train_loader), loss_mean, acc_mean * 100, end_time - start_time, lr), flush=True)
+            print("\r>> Epoch: %d[%d/%d]| Loss: %.6f| Acc: %.2f%%| Time: %.4f" %
+                  (i, j + 1, len(train_loader), loss, acc * 100, end_time - start_time), flush=True)
 
 
 def test(model, test_loader, device):
@@ -129,7 +107,7 @@ def test(model, test_loader, device):
             # 后续操作
             acc = get_acc(pred, label)
             acc_total += acc.item()
-            if i % 10 == 0:
+            if i % 20 == 0:
                 acc = acc_total / (i + 1)
                 end_time = time.time()
                 print("\r>> %d/%d| Acc: %.2f%%| Time: %.4f" %
@@ -141,8 +119,29 @@ def test(model, test_loader, device):
                   (i + 1, len(test_loader), acc * 100, end_time - start_time))
 
 
+class LRScheduler:
+    """学习率控制器"""
+
+    def __init__(self, optim, lr_func):
+        self.optim = optim
+        self.lr_func = lr_func
+
+    def step(self, epoch):
+        lr = self.lr_func(epoch)
+        self.optim.param_groups[0]['lr'] = lr
+        return lr
+
+
+def lr_func(epoch):
+    if 0 <= epoch < 8:
+        return 0.01
+    else:
+        return 1e-3
+
+
 def main():
-    epoch = 18
+    epoch = 10
+    save_model_name = "mnist_model.pth"  # 保存的模型名
 
     if torch.cuda.is_available():
         device = torch.device('cuda')
@@ -150,30 +149,32 @@ def main():
         device = torch.device('cpu')
 
     # 1. 数据集
-    train_cifar10 = CIFAR10("./cifar", train=True, transform=trans.ToTensor(), download=True)
-    test_cifar10 = CIFAR10("./cifar", train=False, transform=trans.ToTensor(), download=True)
+    train_mnist = MNIST("./mnist", train=True, transform=trans.ToTensor(), download=True)
+    test_mnist = MNIST("./mnist", train=False, transform=trans.ToTensor(), download=True)
 
-    train_loader = DataLoader(train_cifar10, 32, True, pin_memory=True, num_workers=1)
-    test_loader = DataLoader(test_cifar10, 32, True, pin_memory=True, num_workers=1)
+    train_loader = DataLoader(train_mnist, 32, True, pin_memory=True, num_workers=1)
+    test_loader = DataLoader(test_mnist, 32, True, pin_memory=True, num_workers=1)
     # 若运行报错使用下面的.
-    # train_loader = DataLoader(train_cifar10, 32, True, pin_memory=True)
-    # test_loader = DataLoader(test_cifar10, 32, True, pin_memory=True)
+    # train_loader = DataLoader(train_mnist, 32, True, pin_memory=True)
+    # test_loader = DataLoader(test_mnist, 32, True, pin_memory=True)
 
     # 2. 建立网络、损失、优化器
     model = SimpleCNN().to(device)
+    if os.path.exists(save_model_name):
+        load_params(model, save_model_name)
     loss_fn = nn.CrossEntropyLoss()
-    # optim = torch.optim.SGD(model.parameters(), 1e-2, momentum=0.9, weight_decay=2e-5, nesterov=True)
-    # optim = torch.optim.SGD(model.parameters(), 1e-2, momentum=0.9, weight_decay=2e-5)
-    optim = torch.optim.Adam(model.parameters(), 5e-4, weight_decay=2e-5)
+    optim = torch.optim.SGD(model.parameters(), 1e-2, momentum=0.9, weight_decay=1e-4)
+    lr_scheduler = LRScheduler(optim, lr_func)
     writer = SummaryWriter()
+    # optim = torch.optim.Adam(model.parameters(), 5e-4, weight_decay=1e-4)
 
     # 3. 训练集训练
     print("---------------------- Train")
-    train(model, loss_fn, optim, train_loader, epoch, device, writer)
+    train(model, train_loader, loss_fn, optim, lr_scheduler, writer, epoch, device)
     # 测试集测试
     print("---------------------- Test")
     test(model, test_loader, device)
-    save_params(model, "cifar10_model.pth")
+    save_params(model, save_model_name)
     writer.close()
 
 
--- 分类_examples/rnn_mnist.py
+++ 分类_examples/rnn_mnist.py
@@ -8,45 +8,28 @@ import torch.nn as nn
 import torch
 import time
 import os
+from tensorboardX import SummaryWriter
 
 
-class ConvBNReLU(nn.Sequential):
-    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, bias):
-        super(ConvBNReLU, self).__init__(
-            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=bias),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
-        )
-
-
-class ConvBlock(nn.Sequential):
-    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, bias):
-        super(ConvBlock, self).__init__(
-            ConvBNReLU(in_channels, out_channels, kernel_size, 1, padding, bias=bias),
-            ConvBNReLU(out_channels, out_channels, kernel_size, 1, padding, bias=bias),
-            ConvBNReLU(out_channels, out_channels, kernel_size, stride, padding, bias=bias)
-        )
-
-
-class SimpleCNN(nn.Module):
+class SimpleRNN(nn.Module):
     """VGG like"""
 
     def __init__(self):
-        super(SimpleCNN, self).__init__()
-        self.features = nn.Sequential(  # 特征提取器
-            ConvBlock(1, 16, 3, 2, 1, False),  # 14
-            ConvBlock(16, 64, 3, 2, 1, False)  # 7
-        )
+        super(SimpleRNN, self).__init__()
+        self.features = nn.LSTM(28, 64, 2, True, True, 0., False)
         self.classifier = nn.Sequential(  # 分类器
-            nn.Linear(7 * 7 * 64, 512),
+            nn.Linear(64 * 28, 512),
             nn.ReLU(inplace=True),
-            nn.Dropout(0.25),  # drop_p
+            nn.Dropout(0.2),  # drop_p
             nn.Linear(512, 10),
         )
 
     def forward(self, x):
-        x = self.features(x)
+        x = x.reshape((-1, 28, 28))
+        x, _ = self.features(x)  # x: shape(N, T, Ch)
         x = torch.flatten(x, start_dim=1)
+        # 或只使用最后一个T, 相应要修改self.classifier第一个linear的in_channels
+        # x = x[:, -1, :]
         x = self.classifier(x)
         return x
 
@@ -64,7 +47,7 @@ def get_acc(pred, label):
     return torch.mean((pred == label).float())
 
 
-def train(model, train_loader, loss_fn, optim, lr_scheduler, epoch, device):
+def train(model, train_loader, loss_fn, optim, lr_scheduler, writer, epoch, device):
     for i in range(epoch):
         loss_total, acc_total, start_time = 0., 0., time.time()
         lr_scheduler.step(i)
@@ -79,6 +62,8 @@ def train(model, train_loader, loss_fn, optim, lr_scheduler, epoch, device):
             acc = get_acc(pred, label)
             loss_total += loss.item()
             acc_total += acc.item()
+            writer.add_scalar("loss", loss, i * len(train_loader) + j + 1)
+            writer.add_scalar("acc", acc, i * len(train_loader) + j + 1)
             if j % 20 == 0:
                 loss = loss_total / (j + 1)
                 acc = acc_total / (j + 1)
@@ -138,7 +123,7 @@ def lr_func(epoch):
 
 def main():
     epoch = 10
-    save_model_name = "mnist_model.pth"  # 保存的模型名
+    save_model_name = "mnist_rnn_model.pth"  # 保存的模型名
 
     if torch.cuda.is_available():
         device = torch.device('cuda')
@@ -156,21 +141,23 @@ def main():
     # test_loader = DataLoader(test_mnist, 32, True, pin_memory=True)
 
     # 2. 建立网络、损失、优化器
-    model = SimpleCNN().to(device)
+    model = SimpleRNN().to(device)
     if os.path.exists(save_model_name):
         load_params(model, save_model_name)
     loss_fn = nn.CrossEntropyLoss()
     optim = torch.optim.SGD(model.parameters(), 1e-2, momentum=0.9, weight_decay=1e-4)
+    writer = SummaryWriter()
     lr_scheduler = LRScheduler(optim, lr_func)
     # optim = torch.optim.Adam(model.parameters(), 5e-4, weight_decay=1e-4)
 
     # 3. 训练集训练
     print("---------------------- Train")
-    train(model, train_loader, loss_fn, optim, lr_scheduler, epoch, device)
+    train(model, train_loader, loss_fn, optim, lr_scheduler, writer, epoch, device)
     # 测试集测试
     print("---------------------- Test")
     test(model, test_loader, device)
     save_params(model, save_model_name)
+    writer.close()
 
 
 if __name__ == '__main__':
